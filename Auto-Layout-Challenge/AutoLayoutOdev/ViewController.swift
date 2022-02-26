087e27d1c87a34bb99df3c6f1f1ca4a43176e735
Bilge Çakar <36795459+bilgecakar@users.noreply.github.com>
2022-02-26 13:35:08 +0300

    Initial commit

---
+++ Auto-Layout-Challenge/AutoLayoutOdev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutOdev
+//
+//  Created by Bilge Çakar on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
